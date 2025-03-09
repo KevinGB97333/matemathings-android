2363d64dd76dcc80ea33bb970e4456e4e80546b5
KevinGB97333 <kevinguarnizobetancourt@gmail.com>
2025-03-09 20:03:04 +0100

    Upgrade dependencies and downgrade java to 17

--- MatemaThings/app/src/main/java/es/upm/tfg/matemathings/activities/MainActivity.kt
+++ MatemaThings/app/src/main/java/es/upm/tfg/matemathings/activities/MainActivity.kt
@@ -1,24 +1,21 @@
 package es.upm.tfg.matemathings.activities
 
-import android.content.pm.ActivityInfo
-import android.database.Observable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import es.upm.tfg.matemathings.R
 import es.upm.tfg.matemathings.databinding.ActivityMainBinding
-import kotlin.properties.Delegates
 
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 	private lateinit var binding : ActivityMainBinding
 	private lateinit var navController: NavController
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-
 		binding = ActivityMainBinding.inflate(layoutInflater)
 		val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 		navController = navHostFragment.navController
