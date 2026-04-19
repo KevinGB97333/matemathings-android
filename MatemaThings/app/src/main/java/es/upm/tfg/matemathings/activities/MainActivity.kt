9ce397e537d44915744d69d9c330bd5c001280e0
KevinGB97333 <kevinguarnizobetancourt@gmail.com>
2026-04-19 20:47:27 +0200

    Fix system bars overlap

--- MatemaThings/app/src/main/java/es/upm/tfg/matemathings/activities/MainActivity.kt
+++ MatemaThings/app/src/main/java/es/upm/tfg/matemathings/activities/MainActivity.kt
@@ -1,8 +1,11 @@
 package es.upm.tfg.matemathings.activities
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.ViewCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import es.upm.tfg.matemathings.R
@@ -10,14 +13,30 @@ import es.upm.tfg.matemathings.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-	private lateinit var binding : ActivityMainBinding
-	private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-		binding = ActivityMainBinding.inflate(layoutInflater)
-		val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-		navController = navHostFragment.navController
-	}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Add System Bars Margin
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            ViewCompat.setOnApplyWindowInsetsListener(binding.mainLayout.rootView) { view, insets ->
+                val systemBars =
+                    insets.getInsets(WindowInsets.Type.systemBars())
+                view.setPadding(
+                    systemBars.left,
+                    systemBars.top,
+                    systemBars.right,
+                    systemBars.bottom
+                )
+                insets
+            }
+        }
+    }
 }
